3f98c980b68d2f667eaed7f6dd2841a7e201195c
Sergii <svbulgakov75@gmail.com>
2023-07-10 17:38:10 +0300

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -29,7 +29,7 @@ export class ContactForm extends Component {
           <Input
             type="text"
             name="name"
-            pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             placeholder="Enter name"
